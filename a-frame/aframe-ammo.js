3d8ab7917114f8b21f3ff6fd19c0cd74fb2c25c1
Jerome Etienne <jerome.etienne@gmail.com>
2017-01-14 14:01:56 +0000

    more work

--- a-frame/aframe-ammo.js
+++ a-frame/aframe-ammo.js
@@ -3,10 +3,9 @@
 //////////////////////////////////////////////////////////////////////////////
 
 AFRAME.registerSystem('ammo-world', {
-	schema: {
-	},
 	init: function () {
                 var ammoWorld = new THREEx.AmmoWorld()
+		// ammoWorld.collisionEnabled = true
 		this.ammoWorld = ammoWorld
 	},
 	tick: function (now, delta) {
@@ -17,13 +16,51 @@ AFRAME.registerSystem('ammo-world', {
 AFRAME.registerComponent('ammo-controls', {
         dependencies: ['ammo-world'],
 	schema: {
+                mass : {
+                        type: 'number',
+                        default: NaN,
+                },
+                restitution : {
+                        type: 'number',
+                        default: 0.9,
+                },
+                friction : {
+                        type: 'number',
+                        default: 0.99,
+                },
+                angularVelocity : {
+                        type: 'vec3',
+                        default: "0 0 0",
+                },
+                linearVelocity : {
+                        type: 'vec3',
+                        default: "0 0 0",
+                },
 	},
 	init: function () {
-                
-                var ammoControls = new THREEx.AmmoControls(this.el.object3D)
+                console.log('data', this.data)
+                // build ammo-controls options
+                var options = {}
+                if( isNaN(this.data.mass) === false ){
+                        options.mass = this.data.mass
+                }
+                // create the ammo-controls
+                var ammoControls = new THREEx.AmmoControls(this.el.object3D, options)
 		this.ammoControls = ammoControls
-
+                
+                // add this controls to ammoWorld
                 var ammoWorld = this.el.sceneEl.systems['ammo-world'].ammoWorld
                 ammoWorld.add(ammoControls)
+
+                var vector3 = this.data.linearVelocity
+                console.log('linearVelocity', vector3)
+                ammoControls.setLinearVelocity(vector3.x, vector3.y, vector3.z)
+
+                var vector3 = this.data.angularVelocity
+                ammoControls.setAngularVelocity(THREE.Math.degToRad(vector3.x), THREE.Math.degToRad(vector3.y), THREE.Math.degToRad(vector3.z))
 	},
+        update: function(){
+                this.ammoControls.setRestitution(this.data.restitution)
+                this.ammoControls.setFriction(this.data.friction)
+        }
 });
